3be3248b39378a7fe1ce7b910b6d9c0c42f3326d
p1809208 <younes.derbal@etu.univ-lyon1.fr>
2021-03-27 23:51:25 +0100

    tout les middle wire il marche il reste le axios pour l'ethentification

--- api/index.js
+++ api/index.js
@@ -1,17 +1,19 @@
+// eslint-disable-next-line no-undef
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3376
+// eslint-disable-next-line no-undef
+let api = require('./routes/api')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use('/api', api)
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
 
 app.use('/static', express.static('public'))
 
 app.use(function (req, res, next) {
     res.status(404).send("Sorry can't find that!")
-  })
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
